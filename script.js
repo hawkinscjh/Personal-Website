f1b395162534f150be27d2594b126d6e671b1ba0
hawkinscjh <hawkinscjh@gmail.com>
2022-06-21 21:55:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const header = document.querySelector('.navbar');
 window.onscroll = function() {
     var top = window.scrollY;
     if(top >=100) {
-        header.classList.add('navbarDark');
+        header.classList.remove('navbarDark');
     }
     else {
-        header.classList.add('navbarDark');
+        header.classList.remove('navbarDark');
     }
 }
